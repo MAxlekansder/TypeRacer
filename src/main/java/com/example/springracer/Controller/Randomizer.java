5a38e832febc7c09bed2ca8e9faf9814c65d2e45
Jonas Ullvahn <ullvahn@gmail.com>
2024-01-11 12:50:18 +0100

    add randomizer method

--- src/main/java/com/example/springracer/Controller/Randomizer.java
+++ src/main/java/com/example/springracer/Controller/Randomizer.java
@@ -1,4 +1,12 @@
 package com.example.springracer.Controller;
 
+import java.util.Random;
+
 public class Randomizer {
+
+    private int generateNumber() {
+        Random random = new Random();
+        return random.nextInt(1,159);
+    }
+
 }
